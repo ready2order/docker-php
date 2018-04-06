96e115b95c83961bfc66968d495502053fdfe6c8
Christoph Herzog <chris@theduke.at>
2018-04-06 18:26:46 +0200

    CI fix

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 REPO=ready2order/php
 USAGE="./build.sh NAME (VERSION|--git-hash) [--push]"
 
+# Ensure that script auto-stops on errors.
+set -euxo pipefail
+
 echo "$@"
 
 NAME=$1
 VERSION=$2
 PUSH=$3
+LOGIN=$4
 
 if [ -z "$NAME" ]; then
     echo $USAGE
@@ -17,14 +21,17 @@ fi
 DOCKERFILE=""
 
 case "$NAME" in
-    fpm-alpine) DOCKERFILE=DockerfileFPMAlpine
+    fpm-alpine)
+        DOCKERFILE=DockerfileFPMAlpine
         ;;
-    *) echo "Invalid name: $NAME" && exit 1
+    *)
+        echo "Invalid name: $NAME"
         ;;
 esac
 
 if [ -z "$VERSION" ]; then
     echo $USAGE
+    exit 1
 fi
 
 if [ "$VERSION" == "--git-hash" ]; then
@@ -33,11 +40,14 @@ fi
 
 FULL_TAG="$REPO:$NAME-$VERSION"
 
-docker build -t $FULL_TAG -f $DOCKERFILE . || exit 1
+if [ ""$LOGIN" == "--login" ]; then
+    echo $DOCKER_PW | docker login -u $DOCKER_USER  --password-stdin
+fi
+
+docker build -t $FULL_TAG -f $DOCKERFILE .
 
 if [ "$3" == "--push" ]; then
     echo "Pushing image with tag $FULL_TAG..."
     docker push $FULL_TAG || exit 1
 fi
-
 
