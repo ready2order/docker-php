e55e8a0f797242739c8f9ac8a62e533e02dfb4b6
Christoph Herzog <chris@theduke.at>
2018-04-07 09:31:18 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ if [ "$LOGIN" == "--login" ]; then
     echo $DOCKER_PW | docker login -u $DOCKER_USER  --password-stdin
 fi
 
-docker build -t $REPO:7.2.4-cli-stretch -f 7.2/stretch/cli/Dockerfile .
-docker build -t $REPO:7.2.4-fpm-stretch -f 7.2/stretch/fpm/Dockerfile .
+docker build -t $REPO:7.2.4-cli -f 7.2/stretch/cli/Dockerfile .
+docker build -t $REPO:7.2.4-fpm -f 7.2/stretch/fpm/Dockerfile .
 
 if [ "$PUSH" == "--push" ]; then
     echo "Pushing image with tag $FULL_TAG...";
